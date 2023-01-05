45e4a9730871c48f93263a997d845ae3c54f5de6
rat soni <andryll.silva@outlook.com>
2023-01-05 14:20:32 -0300

    adicionado a section skills e js para roleta de habilidades

--- src/js/index.js
+++ src/js/index.js
@@ -384,3 +384,18 @@ var VanillaTilt = (function () {
     );
   })();
   
+
+  $(document).ready(function(){
+    if(!$("#myCanvas").tagcanvas({
+      textColour: "rgb(18, 137, 20)",
+      outlineColour: "transparent",
+      reverse: true,
+      depth: 0.8,
+      maxSpeed: 0.05,
+      weight: true,
+
+    }, "tags")){
+      // something went wrong hide the canvas container,
+      $("#myCanvasContainer");
+    }
+  })
